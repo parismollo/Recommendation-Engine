4ff1c51d4ee4a415d18143c3f29dc31e9c7d8422
Paris <parismollochristondis@gmail.com>
2020-04-07 02:56:48 +0200

    recommends produts

--- main.py
+++ main.py
@@ -1,22 +1,20 @@
-from product_recommendantion_engine import create_product_dictionary, create_user_dictionary, fill_recommendations, get_product_recommendation
-
 import sys
+from product_recommendantion_engine import RecommendationModel
 
-filename = 'example.txt'
+
+def run():
+    filename = 'data.txt'
+    products = []
+
+    if len(sys.argv) > 1:
+        for arg in sys.argv[1:]:
+            products.append(arg.upper())
+    else:
+        raise TypeError('Missing arguments. Please type one or multiple products\n Ex: main.py AAA\n')
+
+    re = RecommendationModel(filename)
+    print(f"\n", *re.recommend_product(products), "\n", sep=' ')
 
 
-with open(filename) as f:
-    users = create_user_dictionary(f)
-
-print(f"\nUSERS:\n{users}\n")
-
-a = 'Apple'
-b = 'Date'
-for user in users:
-    if a in users[user] and b in users[user]:
-        print(f"{user}\n")
-# recommendations = create_product_dictionary(users)
-# fill_recommendations(recommendations, users)
-
-# for product in sys.argv[1:]:
-#     get_product_recommendation(product, recommendations)
+if __name__ == "__main__":
+    run()
--- product_recommendantion_engine.py
+++ product_recommendantion_engine.py
@@ -1,34 +1,39 @@
-
-def create_user_dictionary(file):
-    users = {}
-    for line in file:
-        x = line.split()[0]
-        products = []
-        for p in line.split()[1:]:
-            products.append(p)
-            users[x] = products
-    return users
+from tqdm import tqdm
+from typing import Dict, List
 
 
-def create_product_dictionary(users):
-    recommendations = {}
-    for user in users:
-        for p in users[user]:
-            recommendations[p] = []
-    return recommendations
+class RecommendationModel:
+    def __init__(self, filename) -> None:
+        self.users: Dict[str, List[str]] = {}
+        self.filename = filename
 
+    def create_user_sets(self) -> None:
+        with open(self.filename) as f:
+            print("\nCreating Dictionary...\n")
+            for line in tqdm(f):
+                user = line.split()[0]
+                products = []
+                for p in line.split()[1:]:
+                    products.append(p)
+                    self.users[user] = products
 
-def fill_recommendations(recommendations, users):
-    for product in recommendations:
-        x = []
-        for user in users:
-            if product in users[user]:
-                for other_product in users[user]:
-                    if product != other_product:
-                        x.append(other_product)
+    def flatter(self, recommendations) -> set:
+        flat_list_recommendations = [item for sublist in recommendations for item in sublist]
+        return set(flat_list_recommendations)
 
-        recommendations[product] = set(x)
+    def recommend_product(self, products) -> set:
+        self.create_user_sets()
+        products = list(products)
+        recommendations = []
 
+        print("\nLooking for recommendations...\n")
 
-def get_product_recommendation(product, recommendations):
-    print(f"\nFor the product {product}, We recommend the following:\n {recommendations[product]}\n")
+        for user in tqdm(self.users):
+            if set(products).issubset(self.users[user]):
+                recommended_product = list(set(self.users[user]) - set(products))
+                if len(recommended_product) > 0:
+                    recommendations.append(recommended_product)
+
+        if len(recommendations) == 0:
+            print(f'\nNo recommendations for {products} so far...')
+        return self.flatter(recommendations)
